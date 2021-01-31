960dc4585dade0fc64d2854343ae61fdd05a2115
abewallah <ibrahimabdull93@gmail.com>
2021-01-30 18:53:34 -0800

    Bio schema Created

--- models/Bio.js
+++ models/Bio.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const bioSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    fName: {
+      type: String,
+    },
+    lName: {
+      type: String,
+    },
+    DOB: {
+      type: String,
+      required: true,
+    },
+    nickName: String,
+  },
+  { timestamps: true }
+);
 
-})
+const Bio = mongoose.model('Bio', bioSchema);
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = Bio;
