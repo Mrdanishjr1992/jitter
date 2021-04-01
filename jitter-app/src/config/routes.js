b6d273dc19cc958773fe07672319967f8de8a2a6
whobutjg <jgiardinajg@gmail.com>
2021-04-01 18:41:21 -0400

    Adds profile route with profile page

--- jitter-app/src/config/routes.js
+++ jitter-app/src/config/routes.js
@@ -2,11 +2,13 @@ import { Switch, Route } from 'react-router-dom';
 import React from 'react';
 
 import MainContainer from '../container/MainContainer';
+import Profile from '../pages/profile/Profile';
 
 function Routes({ token, setToken }) {
   return (
     <Switch>
       <Route exact path='/' component={MainContainer} />
+      <Route path='/profile' component={Profile} />
     </Switch>
   );
 }
--- jitter-app/src/universalComponents/SideCar.js
+++ jitter-app/src/universalComponents/SideCar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SideCar = () => {
   return (
@@ -19,7 +20,7 @@ const SideCar = () => {
             <a href='/#'>Messages</a>
           </li>
           <li>
-            <a href='/#'>Profile</a>
+            <Link to='/profile'>Profile</Link>
           </li>
         </nav>
       </aside>
