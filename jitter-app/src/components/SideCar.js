6ea99f3a801e63dd036760e9e82518425194a065
mrdanishjr1992 <mrdanishjr1992@gmail.com>
2021-07-07 18:58:56 -0500

    minor changes to home page

--- jitter-app/src/components/SideCar.js
+++ jitter-app/src/components/SideCar.js
@@ -4,26 +4,24 @@ const SideCar = () => {
   return (
     <div className='sidecar'>
       <h1 className='title'>Jitter</h1>
-      <aside>
-        <nav>
-          <li>
-            <a href='/#'>Home</a>
-          </li>
-          <li>
-            <a href='/#'>Explore</a>
-          </li>
-          <li>
-            <a href='/#'>Notifications</a>
-          </li>
-          <li>
-            <a href='/#'>Messages</a>
-          </li>
-          <li>
-            <a href='/#'>Profile</a>
-          </li>
-        </nav>
-      </aside>
-      <button>Post</button>
+      <nav className='sideCont'>
+        <li>
+          <a href='/#'>Home</a>
+        </li>
+        <li>
+          <a href='/#'>Explore</a>
+        </li>
+        <li>
+          <a href='/#'>Notifications</a>
+        </li>
+        <li>
+          <a href='/#'>Messages</a>
+        </li>
+        <li>
+          <a href='/#'>Profile</a>
+        </li>
+      </nav>
+      <button className='btn btn-outline-success'>Post</button>
     </div>
   );
 };
